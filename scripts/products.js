b815a860341e9d084463dfbb26c9cbc9d495bd88
Kaan Erol <kaanerol395@gmail.com>
2024-12-11 17:53:54 +0300

    Use products.js file

---
+++ scripts/products.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        title: "It's a pumpkin party",
+        text: "The season's favorite makes fall fall. Enjoy the moment—hot, iced or blended.",
+        button: "Order Now",
+        image: "img/img1.jpg",
+        backgroundColor: "rgb(250, 116, 92)"
+    },
+    {
+        title: "Send a scary - delicious treat",
+        text: "Share the thrill of Halloween by treating someone special to a eGift.",
+        button: "Send an eGift",
+        image: "img/img2.jpg",
+        backgroundColor: "rgb(102, 30, 66)"
+    },
+    {
+        title: "Merrily delicious",
+        text: "Grab a toasty warm sandwich or a new Turkey Sage Danish and make your afternoon feel oh-so bright.",
+        button: "Order Now",
+        image: "img/img3.jpg",
+        backgroundColor: "rgb(6, 206, 141)"
+    }
+];
