49d5e10c9acb5e36f1db1cb607620a6f4707591d
Kaan Erol <kaanerol395@gmail.com>
2025-01-21 19:06:33 +0300

    Removed unused footer.html and footer.js files

--- scripts/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('../footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
-        })
-        .catch(error => console.log('Footer error: ', error));
-});
